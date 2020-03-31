a9ead29183444fbb8a6a0ce90974d30f4190a42e
Flav1us <an215@ukr.net>
2020-03-31 22:06:37 +0300

    simple sign in / sign up page with flask+sqlite3

---
+++ flask_hello/src/app.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired 
+
+import mydb
+import sqlite3
+
+app = Flask(__name__)
+
+CSRF_ENABLED = True
+app.config['SECRET_KEY'] = "sdxLWlkwejWLEKwvbsmdXXMCVBWEUwefoimHf"
+
+
+
+print(sqlite3.connect("test.db").cursor().execute("select * from users").fetchall())
+
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+
+class MyForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+
+class MyFormWithEmail(MyForm):
+    email = StringField('email', validators=[DataRequired()])
+
+@app.route('/')
+def index():
+    return render_template('index.html', form=MyForm())
+
+@app.route('/login', methods=['POST'])
+def submit_login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if mydb.areValidCredentials(form.username.data, form.password.data):
+            return "success login"
+        else:    
+            return "such user does not exist"
+    else:
+        print("form invalid")
+        print(form.errors)
+        return render_template('index.html', form=form)
+
+@app.route('/signup', methods=['POST'])
+def submit_signup():
+    form = MyFormWithEmail()
+    if form.validate_on_submit():
+        if mydb.containsUser(form.username.data):
+            return "user already exist"
+        else:   
+            mydb.createUser(form.username.data, form.password.data, form.email.data)
+            return "success signup"
+    else:
+        print("form invalid")
+        print(form.errors)
+        return render_template('index.html', form=form)
---
+++ flask_hello/src/mydb.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+def containsUser(username):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute("select username from Users where username=?", [(username)])
+    res = cursor.fetchall().__len__() > 0
+    conn.commit()
+    conn.close()
+    return res
+
+def createUser(username, password, email):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute("insert into Users values (?, ?, ?)", (username, password, email))
+    conn.commit()
+    conn.close()
+    return 1
+
+def areValidCredentials( username, password):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    cursor.execute("select * from Users where username=? and password=?", (username, password))
+    res = cursor.fetchall().__len__() > 0
+    conn.commit()
+    conn.close()
+    return res
