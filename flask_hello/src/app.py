0fc305e9a4c62007bfcccef4f3cf30170238b108
Flav1us <an215@ukr.net>
2020-04-04 18:45:36 +0300

    first try

--- flask_hello/src/app.py
+++ flask_hello/src/app.py
@@ -1,18 +1,44 @@
 from flask import Flask
 from flask import render_template
 from flask_wtf import FlaskForm
+from flask_user import UserManager, login_required
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 from db.user_db import UserDb
+from db.models import User
 from logger import log
 
+
+# Class-based application configuration
+class ConfigClass(object):
+    """ Flask application config """
+
+    # Flask settings
+    SECRET_KEY = 'sdxLWlkwejWLEKwvbsmdXXMCVBWEUwefoimHf'
+
+    # Flask-SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'    # File-based SQL database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
+
+    # Flask-User settings
+    USER_APP_NAME = "Flask-User test"      # Shown in and email templates and page footers
+    USER_ENABLE_EMAIL = False      # Disable email authentication
+    USER_ENABLE_USERNAME = True    # Enable username authentication
+    USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
+
+
 app = Flask(__name__)
+app.config.from_object(__name__+'.ConfigClass')
 
 CSRF_ENABLED = True
-app.config['SECRET_KEY'] = "sdxLWlkwejWLEKwvbsmdXXMCVBWEUwefoimHf"
+# app.config['SECRET_KEY'] = "sdxLWlkwejWLEKwvbsmdXXMCVBWEUwefoimHf"
 
+sqla_db = SQLAlchemy(app)
 mydb = UserDb()
+
+user_manager = UserManager(app, sqla_db, User)
 
 
 class MyForm(FlaskForm):
@@ -79,3 +105,9 @@ def submit_signup():
         log(log.ERROR, 'form invalid')
         log(log.ERROR, form.errors)
         return render_template('index.html', form=form)
+
+
+@app.route('/cabinet')
+@login_required
+def get_cabinet():
+    return render_template('cabinet.html', username='user')
--- flask_hello/src/db/models.py
+++ flask_hello/src/db/models.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from flask_user import UserMixin
 from db.session import db_engine
+
 
 ModelBase = declarative_base(bind=db_engine)
 
 
-class User(ModelBase):
-
+# UserMixin - класс из flask_user для аутентификации
+class User(ModelBase, UserMixin):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
